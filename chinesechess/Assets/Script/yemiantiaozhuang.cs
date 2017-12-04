7fc606907fab78b0380ae9dd2b194fd94e346e33
qinzheng <337248614@qq.com>
2017-12-04 09:15:33 +0800

    更新UI

--- chinesechess/Assets/Script/yemiantiaozhuang.cs
+++ chinesechess/Assets/Script/yemiantiaozhuang.cs
@@ -5,14 +5,19 @@ using UnityEngine.SceneManagement;
 public class yemiantiaozhuang : MonoBehaviour {
 	public static int ChessPeople;//判断当前是人人象棋 还是人机
 	// Use this for initialization
-	public GameObject lab,sprite;
+	public GameObject BtnPosition;
     public GameObject difficultselect;
-    public UILabel Lab;
+    UILabel Lab;
 	void Awake () {
 	//把难度选择 和 new label  隐藏  人人对战的时候
         GameObject obj = GameObject.Find("NextTips");
         Lab = obj.GetComponent<UILabel>();
-        if (ChessPeople==2) difficultselect.SetActive(false);
+        if (ChessPeople == 2)
+        {
+            difficultselect.SetActive(false);
+            Vector3 UIpos = BtnPosition.transform.localPosition;
+            BtnPosition.transform.localPosition = new Vector3(UIpos.x, UIpos.y + 70, UIpos.z);
+        }
 	}
 
     void Update()
@@ -24,21 +29,6 @@ public class yemiantiaozhuang : MonoBehaviour {
         SceneManager.LoadScene("MainMenu");
     }
 
-    //public void OnCompleteSettingButtonClik(){//返回
-    //    //返回界面  数组重新初始化
-	
-		
-    //    for (int c=ChessChongzhi.Count; c>1; c--) {
-    //        chess.IloveHUIQI();
-    //    }
-    //    for (int i=1; i<=90; i++) {
-    //        GameObject game = GameObject.Find("item"+i.ToString());
-    //        GameObject Clear = GameObject.Find("prefabs"+i.ToString());
-    //        Destroy(game);
-    //        Destroy(Clear);
-    //    }
-    //    //what = GameObject.Find ("Musiccup");
-    //    //Destroy (what);
-    //}
+    
 
 }
